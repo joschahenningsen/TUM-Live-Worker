5a794780790ec78f71e9c4eec938f61c28d83c5f
Joscha Henningsen <joscha.henningsen@tum.de>
2021-04-03 13:13:46 +0200

    use curl

--- convert_upload.go
+++ convert_upload.go
@@ -6,9 +6,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"mime/multipart"
 	"net/http"
-	"os"
 	"os/exec"
 	"strings"
 	"time"
@@ -18,7 +16,7 @@ func convertAndUpload() {
 	if Busy {
 		return
 	}
-	res, err := http.Get("http://backend:7002/api/worker/getJobs/" + Cfg.WorkerID)
+	res, err := http.Get("http://localhost:8080/api/worker/getJobs/" + Cfg.WorkerID)
 	if err != nil {
 		log.Printf("couldn't get jobs%v\n", err)
 		return
@@ -43,49 +41,48 @@ func convertAndUpload() {
 
 	convert(job.Path)
 	newPath := strings.Replace(job.Path, ".flv", ".mp4", 1)
-	upload(newPath, job)
+	if job.Upload {
+		upload(newPath, job)
+	}
 	Busy = false
 
 }
 
 func upload(path string, job jobData) {
 	log.Printf("Uploading %v", path)
+	pathparts := strings.Split(path, "/")
+	cmd := exec.Cmd{
+		Path: "/bin/curl",
+		Args: []string{
+			"-F", fmt.Sprintf("'benutzer=%v'", Cfg.LrzUser),
+			"-F", fmt.Sprintf("'mailadresse=%v'", Cfg.LrzMail),
+			"-F", fmt.Sprintf("'telefon=%v'", Cfg.LrzPhone),
+			"-F", "'unidir=tum'",
+			"-F", fmt.Sprintf("'subdir=%v'", Cfg.LrzSubDir),
+			"-F", fmt.Sprintf("'info='"),
+			"-F", fmt.Sprintf("'filename=@%v'", path),
+			Cfg.LrzUploadURL,
+		},
+		Dir: "/recordings/vod",
+	}
+	err := cmd.Start()
+	if err != nil {
+		log.Printf("Error starting curl: %v", err)
+	}
+	err = cmd.Wait()
 
-	extraParams := map[string]string{
-		"benutzer":       Cfg.LrzUser,
-		"mailadresse":      Cfg.LrzMail,
-		"telefon": Cfg.LrzPhone,
-		"unidir": "tum",
-		"subdir": Cfg.LrzSubDir,
-		"info": "testupload",
-	}
-	request, err := newfileUploadRequest(Cfg.LrzUploadURL, extraParams, "filename", path)
 	if err != nil {
-		log.Fatal(err)
-	}
-	client := &http.Client{}
-	resp, err := client.Do(request)
-	fmt.Println(request.PostForm.Encode())
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		var bodyContent []byte
-		fmt.Println(resp.StatusCode)
-		fmt.Println(resp.Header)
-		resp.Body.Read(bodyContent)
-		resp.Body.Close()
-		fmt.Println(string(bodyContent))
+		log.Printf("Error executing curl: %v", err)
 	}
 	log.Println("Uploaded file to lrz.")
-	pathparts := strings.Split(path, "/")
 	createVodData := putVodData{
 		Name:     job.Name,
 		Start:    job.StreamStart,
-		HlsUrl:   "https://stream.lrz.de/vod/_definst_/mp4:tum/RBG/1." + pathparts[len(pathparts)-1] + "/playlist.m3u8",
+		HlsUrl:   "https://stream.lrz.de/vod/_definst_/mp4:tum/RBG/" + pathparts[len(pathparts)-1] + "/playlist.m3u8",
 		StreamId: job.StreamId,
 	}
 	send, _ := json.Marshal(createVodData)
-	_, err = http.Post("http://backend:7002/api/worker/putVOD/"+Cfg.WorkerID,
+	_, err = http.Post("http://backend:8080/api/worker/putVOD/"+Cfg.WorkerID,
 		"application/json",
 		bytes.NewBuffer(send))
 	if err != nil {
@@ -110,47 +107,13 @@ func convert(file string) {
 	// _ = os.Remove(file)
 }
 
-// Creates a new file upload http request with optional extra params
-func newfileUploadRequest(uri string, params map[string]string, paramName, path string) (*http.Request, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	fileContents, err := ioutil.ReadAll(file)
-	if err != nil {
-		return nil, err
-	}
-	fi, err := file.Stat()
-	if err != nil {
-		return nil, err
-	}
-	_ = file.Close()
-
-	body := new(bytes.Buffer)
-	writer := multipart.NewWriter(body)
-	part, err := writer.CreateFormFile(paramName, fi.Name())
-	if err != nil {
-		return nil, err
-	}
-	_, _ = part.Write(fileContents)
-
-	for key, val := range params {
-		_ = writer.WriteField(key, val)
-	}
-	err = writer.Close()
-	if err != nil {
-		return nil, err
-	}
-
-	return http.NewRequest("POST", uri, body)
-}
-
 type jobData struct {
 	Id          uint      `json:"id"`
 	Name        string    `json:"name"`
 	StreamId    uint      `json:"streamId"`
 	StreamStart time.Time `json:"streamStart"`
 	Path        string    `json:"path"`
+	Upload      bool
 }
 
 type putVodData struct {
